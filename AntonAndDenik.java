599b72ef696763eae3e9617076f08bbe1b85fee2
Imran Hossain <50188662+HossainImran19@users.noreply.github.com>
2019-11-21 22:26:26 +0600

    Add files via upload

---
+++ AntonAndDenik.java
@@ -0,0 +1,30 @@
+package imran.code.forces;
+import java.util.*;
+
+public class AntonAndDenik {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		String st = s.next();
+		int n = 0;
+		
+		
+	
+		while(a - 1>0) {
+			int m = 0;
+			int b = st.charAt(m);
+			if(b == 65) {
+				n++;
+			}else {
+				continue;
+			}
+			m++;
+			a--;
+		}
+		System.out.println(n == (a-n)?"Friendship":n > (a-n)?"Anton":"Danik"); 
+				
+
+	}
+
+}
